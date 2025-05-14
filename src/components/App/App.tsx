d3677d4748b057f79bbd1c10b6fc550025d8450d
Yuri Boblak <160606809+sselesnes@users.noreply.github.com>
2025-05-14 08:57:36 +0300

    styles

---
+++ src/components/App/App.tsx
@@ -0,0 +1,13 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+
+import css from "./App.module.css";
+
+export default function App() {
+  return (
+    <div className={css.container}>
+      <p className={css.text}>Hello App</p>
+    </div>
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./components/App.tsx";
+import App from "./components/App/App";
+import "./css/styles.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
